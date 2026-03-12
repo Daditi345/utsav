b12051ac997420422dd6e3b1cd5c71a3d45706d3
Abhinav Shukla <abhinav.sh226@gmail.com>
2026-03-12 23:33:52 +0530

    response messsage update

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def default_path():
-    return "Hello you are connected with your application"
+    return "Hello World! Welcome to FastAPI"
 
 @app.get("/hello/{name}/{address}/{contact}")
 def hello(name: str, address: str, contact: str):
